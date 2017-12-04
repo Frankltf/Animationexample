e9b6523afafac342fa314df7217bf61349311ba4
langerfei@163.com <langerfei@163.com>
2017-12-04 11:26:39 +0800

    11

--- AnimationExample/AnimationExample/ViewController.m
+++ AnimationExample/AnimationExample/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #define SCREEN_H [UIScreen mainScreen].bounds.size.height
 #define SCREEN_W [UIScreen mainScreen].bounds.size.width
-@interface ViewController ()
+@interface ViewController ()<CAAnimationDelegate>
 @property (weak, nonatomic) IBOutlet UIView *animationview;
 
 @end
@@ -45,7 +45,7 @@
     anima.fromValue=[NSNumber numberWithFloat:1];
     anima.toValue=[NSNumber numberWithFloat:0.2];
     anima.duration=1;
-    [self.animationview.layer addAnimation:anima forKey:@"opacityAnimation"];
+    [self.animationview.layer addAnimation:anima forKey:@"opacirtyAnimation"];
 }
 - (IBAction)xuanzhuan:(UIButton *)sender {
     CABasicAnimation *anima=[CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
@@ -66,8 +66,30 @@
     [self.animationview.layer addAnimation:anima forKey:@"scaleAnimation"];
 }
 - (IBAction)guanjianzhen:(UIButton *)sender {
+    CAKeyframeAnimation *anima=[CAKeyframeAnimation animationWithKeyPath:@"position"];
+    NSValue *value0=[NSValue valueWithCGPoint:CGPointMake(0, SCREEN_H/2-50)];
+    NSValue *value1=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W/3, SCREEN_H/2-50)];
+    NSValue *value2=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W/3, SCREEN_H/2+50)];
+    NSValue *value3=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W*2/3, SCREEN_H/2+50)];
+    NSValue *value4=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W*2/3, SCREEN_H/2-50)];
+    NSValue *value5=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W-30, SCREEN_H/2-50)];
+    anima.values=[NSArray arrayWithObjects:value0,value1,value2,value3,value4,value5 , nil];
+    anima.duration=2;
+    anima.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    anima.delegate=(id)self;
+    [self.animationview.layer addAnimation:anima forKey:@"keyFrameAnimation"];
     
 }
+-(void)animationDidStart:(CAAnimation *)anim
+{
+    NSLog(@"animationDidStart");
+}
+-(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+     NSLog(@"animationDidStop");
+}
+
+
 - (IBAction)lujing:(UIButton *)sender {
     UIView *view=[[UIView alloc]init];
     view.frame=CGRectMake(100,SCREEN_H/2-300, 200, 200);
@@ -77,6 +99,7 @@
     UIBezierPath *path=[UIBezierPath bezierPathWithOvalInRect:CGRectMake(100,SCREEN_H/2-300, 200, 200)];
     anima.path=path.CGPath;
     anima.duration=2;
+    anima.delegate=(id)self;
     [self.animationview.layer addAnimation:anima forKey:@"pathAnimation"];
 }
 - (IBAction)doudong:(UIButton *)sender {
@@ -87,6 +110,54 @@
     anima.values=@[value1,value2,value3];
     anima.repeatCount=MAXFLOAT;
     [self.animationview.layer addAnimation:anima forKey:@"shakeAnimation"];
+}
+- (IBAction)shunxu:(UIButton *)sender {
+    CFTimeInterval currenttime=CACurrentMediaTime();
+    CABasicAnimation *anima1=[CABasicAnimation animationWithKeyPath:@"position"];
+    anima1.fromValue=[NSValue valueWithCGPoint:CGPointMake(0, SCREEN_H/2)];
+    anima1.toValue=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W/2, SCREEN_H/2)];
+    anima1.beginTime=currenttime;
+    anima1.duration=2;
+    anima1.fillMode=kCAFillModeForwards;
+    anima1.removedOnCompletion=NO;
+    [self.animationview.layer addAnimation:anima1 forKey:@"aa"];
+    
+    CABasicAnimation *anima2=[CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    anima2.fromValue=[NSNumber numberWithFloat:0.5f];
+    anima2.toValue=[NSNumber numberWithFloat:2.5f];
+    anima2.beginTime=currenttime+2;
+    anima2.duration=2;
+    anima2.fillMode=kCAFillModeForwards;
+    anima2.removedOnCompletion=NO;
+    [self.animationview.layer addAnimation:anima2 forKey:@"bb"];
+    
+    CABasicAnimation *anima3=[CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    anima3.toValue=[NSNumber numberWithFloat:M_PI*3];
+    anima3.beginTime=currenttime+4;
+    anima3.duration=3;
+    anima3.fillMode=kCAFillModeForwards;
+    anima3.removedOnCompletion=NO;
+    [self.animationview.layer addAnimation:anima3 forKey:@"cc"];
+}
+- (IBAction)tongbu:(UIButton *)sender {
+    CAKeyframeAnimation *anima1=[CAKeyframeAnimation animationWithKeyPath:@"position"];
+    NSValue *value0=[NSValue valueWithCGPoint:CGPointMake(0, SCREEN_H/2-50)];
+    NSValue *value2=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W/3, SCREEN_H/2-50)];
+    NSValue *value3=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W/3, SCREEN_H/2+50)];
+    NSValue *value4=[NSValue valueWithCGPoint:CGPointMake(SCREEN_W*2/3, SCREEN_H/2+50)];
+    anima1.values=[NSArray arrayWithObjects:value0,value2,value3,value4, nil];
+    
+    CABasicAnimation *anima2=[CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    anima2.fromValue=[NSNumber numberWithFloat:0.5];
+    anima2.toValue=[NSNumber numberWithFloat:2];
+    
+    CABasicAnimation *anima3=[CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    anima3.toValue=[NSNumber numberWithFloat:M_PI *4];
+    
+    CAAnimationGroup *groupanimation=[CAAnimationGroup animation];
+    groupanimation.animations=[NSArray arrayWithObjects:anima1,anima2,anima3, nil];
+    groupanimation.duration=4;
+    [self.animationview.layer addAnimation:groupanimation forKey:@"groupAnimation"];
 }
 
 
